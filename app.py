6961b2acd6b6d693d41e065d2321686e93c39402
geofferyj <geofferyjoseph1@gmail.com>
2020-02-05 07:45:07 +0100

    added image upload functionality to the editor

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, redirect, render_template, session, url_for, request, flash
+from flask import Flask, redirect, render_template, session, url_for, request, flash, abort, jsonify, make_response
 from sqlalchemy import create_engine, exc
 from sqlalchemy.orm import sessionmaker, scoped_session
 from flask_session import Session
-import re, datetime
+from werkzeug import secure_filename
+import re, datetime, os
 
 
 
@@ -14,6 +15,9 @@ application = app
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config['SECRET_KEY'] = "efd432e0aca715610c505c533037b95d6fb22f5692a0d33820ab7b19ef06f513"
+app.config['UPLOAD_FOLDER'] = 'static/images/uploads'
+
+app.config['ALLOWED_EXT'] = ['jpg', 'jpeg', 'png', 'gif', ]
 
 db_string = 'postgres+psycopg2://geojoe_geoffery:donsniper123@localhost:5432/geojoe_flask'
 engine = create_engine(db_string)
@@ -142,8 +146,40 @@ def edit_post():
 def post(slug):
 
     picture = ""
-    posts = db.execute("select title, date_published, body, name from posts, users where users.user_id = posts.author and posts.slug = :slug", {"slug": slug})
-    return render_template('post.html', posts=posts, picture=picture)
+    posts = db.execute("select title, date_published, body, name from posts, users where users.user_id = posts.author and posts.slug = :slug", {"slug": slug}).fetchone()
+    return render_template('post.html', post=posts, picture=picture)
+
+def allowed_file(filename):
+    allowed_ext = app.config['ALLOWED_EXT']
+    exp = '.' in filename and filename.rsplit('.', 1)[1].lower() in allowed_ext
+    return exp
+
+
+
+@app.route("/upload/", methods=['POST'])
+def upload():
+    if request.method == 'POST':
+        if request.files:
+            image = request.files['image']
+
+            if image.filename == '':
+                responce_msg = {"success":False, "msg": "no file name", "status":404}
+                res = make_response(jsonify(responce_msg))
+                return res
+            
+            if allowed_file(image.filename):
+                filename = secure_filename(image.filename)
+                image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+                image.save(image_path)
+                responce_msg = {"data": {"link": image_path},"success":True,"status":200}
+                res = make_response(jsonify(responce_msg))
+                return res
+            else:
+                responce_msg = {"success":False, "msg": "invalid file type", "status":404}
+                res = make_response(jsonify(responce_msg))
+                return res
+    
+    return redirect(request.url)
 
 
 
